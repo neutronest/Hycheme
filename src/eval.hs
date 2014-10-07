64bb756a98fc3aa4bba348e95efff06907b9a379
dongchao <dongchao@xiaomi.com>
2014-10-07 16:45:16 +0900

    eval

--- src/eval.hs
+++ src/eval.hs
@@ -11,6 +11,11 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) = do
+                                             result <- eval pred
+                                             case result of
+                                                Bool False -> eval alt
+                                                otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecongnized special form " badForm
 
@@ -23,6 +28,18 @@ apply func args = maybe (throwError $ NotFunction "Unrecongnized primitives func
 
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [
+                ("=", numBoolBinop (==)),
+                ("<", numBoolBinop (<)),
+                (">", numBoolBinop (>)),
+                ("/=", numBoolBinop (/=)),
+                ("<=", numBoolBinop (<=)),
+                (">=", numBoolBinop (>=)),
+                ("&&", boolBoolBinop (&&)),
+                ("||", boolBoolBinop (||)),
+                ("string=?", strBoolBinop(==)),
+                ("string?", strBoolBinop(>)),
+                ("stirng?<=", strBoolBinop(<=)),
+                ("string?>=", strBoolBinop(>=)),
                 ("+", numberBinop (+)),
                 ("-", numberBinop (-)),
                 ("*", numberBinop (*)),
@@ -35,6 +52,19 @@ numberBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispV
 numberBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numberBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
 
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args = if length args /= 2
+    then throwError $ NumArgs 2 args
+    else do
+         left <- unpacker $ args !! 0
+         right <- unpacker $ args !! 1
+         return $ Bool $ left `op` right
+
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+boolBoolBinop = boolBinop unpackBool
+
+
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
 unpackNum (String n) = let parsed = reads n in
@@ -43,3 +73,13 @@ unpackNum (String n) = let parsed = reads n in
                                 else return $ fst $ parsed !! 0
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s) = return $ show s
+unpackStr notString = throwError $ TypeMismatch "string" notString
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
